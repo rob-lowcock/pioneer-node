f51047f8cefb05c9cd1eb6cc96a035eaccd68920
Rob Lowcock <rob.lowcock@gmail.com>
2021-12-24 18:24:20 +0000

    Add support for POSTing retro cards

--- routes/retrocards.js
+++ routes/retrocards.js
@@ -25,10 +25,25 @@ router.get('/:cardId', asyncMiddleware(async function(req, res, next) {
   await client.connect();
 
   const dbCall = await client.query('SELECT id, title, col as column FROM retrocards WHERE id=$1', [req.params.cardId]);
-  var output = dbCall.rows;
+  var output = dbCall.rows[0];
   await client.end();
 
   res.json(output);
+}));
+
+router.post('/', asyncMiddleware(async function(req, res, next) {
+  const client = new Client();
+  await client.connect();
+
+  const dbCall = await client.query('INSERT INTO retrocards (title, col) VALUES ($1, $2) RETURNING (id)', [req.body.title, req.body.column]);
+  var output = dbCall.rows;
+  await client.end();
+
+  res.json({
+    id: output[0].id,
+    title: req.body.title,
+    column: req.body.column,
+  });
 }));
 
 module.exports = router;
